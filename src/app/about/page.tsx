b9196c582b9f0b00a8ed1ac914ce5ceb86e843ab
Ajit Shatvapa Gadkari <ajit@Ajits-MacBook-Pro.local>
2024-08-21 13:51:11 +0530

    completed redirect to tickets if user is already logged in

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,27 @@
 "use client";
 
-const About = () => (
+import pb from "@/lib/pocketbase";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+const About = () => {
+  const [authValid, setIsAuthValid] = useState(pb.authStore.isValid);
+  const router= useRouter()
+  useEffect(() => {
+      const unsubscribe = pb.authStore.onChange(() => {
+          setIsAuthValid(pb.authStore.isValid);
+      });
+
+      // Cleanup the listener on component unmount
+      return () => unsubscribe();
+  }, []);
+if(authValid){
+router.push('/intrendapp/tickets')
+}
+  return(
   <div className="flex items-center justify-center min-h-screen bg-gray-100">
     <h1 className="text-4xl font-bold">About Page</h1>
   </div>
-);
+);}
 
 export default About;
