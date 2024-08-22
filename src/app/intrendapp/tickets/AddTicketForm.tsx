aa0a35c635cb75a12009786840ed20b7d3bae7b7
Ajit Shatvapa Gadkari <ajit@Ajits-MacBook-Pro.local>
2024-08-22 14:06:53 +0530

    added label to drop-down

--- src/app/intrendapp/tickets/AddTicketForm.tsx
+++ src/app/intrendapp/tickets/AddTicketForm.tsx
@@ -113,59 +113,15 @@ const AddTicketForm = ({ onAdd }: AddTicketFormProps) => {
 
   return (
     <form onSubmit={handleSubmit} className="space-y-4">
-      <div className="mb-4">
-        <button
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          type="button"
-          onClick={() => setShowDropDown(!showDropDown)}
-        >
-          {formData.customer_name || "Select Customer"}
-          <svg
-            className="w-2.5 h-2.5 ms-3"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 10 6"
-          >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="m1 1 4 4 4-4"
-            />
-          </svg>
-        </button>
-
-        <div
-          className={`z-10 ${
-            !showDropDown && "hidden"
-          } bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700`}
-        >
-          <ul className="py-2 text-sm text-gray-700 dark:text-gray-200">
-            {customers.map((customer) => (
-              <li
-                key={customer._id}
-                className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white cursor-pointer"
-                onClick={() => {
-                  setFormData({ ...formData, customer_name: customer.name });
-                  setShowDropDown(false);
-                }}
-              >
-                {customer.name}
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
-      {formData.customer_name && (
+      <>
+        <label>Customer</label>
         <div className="mb-4">
           <button
             className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             type="button"
-            onClick={() => setShowPersonDropDown(!showPersonDropDown)}
+            onClick={() => setShowDropDown(!showDropDown)}
           >
-            {formData.person_name || "Select Person"}
+            {formData.customer_name || "Select Customer"}
             <svg
               className="w-2.5 h-2.5 ms-3"
               aria-hidden="true"
@@ -185,31 +141,82 @@ const AddTicketForm = ({ onAdd }: AddTicketFormProps) => {
 
           <div
             className={`z-10 ${
-              !showPersonDropDown && "hidden"
+              !showDropDown && "hidden"
             } bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700`}
           >
             <ul className="py-2 text-sm text-gray-700 dark:text-gray-200">
-              {people.length > 0 ? (
-                people.map((person) => (
-                  <li
-                    key={person._id}
-                    className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white cursor-pointer"
-                    onClick={() => {
-                      setFormData({ ...formData, person_name: person.name });
-                      setShowPersonDropDown(false);
-                    }}
-                  >
-                    {person.name}
-                  </li>
-                ))
-              ) : (
-                <li className="block px-4 py-2 text-gray-500">
-                  No people available for this customer
+              {customers.map((customer) => (
+                <li
+                  key={customer._id}
+                  className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white cursor-pointer"
+                  onClick={() => {
+                    setFormData({ ...formData, customer_name: customer.name });
+                    setShowDropDown(false);
+                  }}
+                >
+                  {customer.name}
                 </li>
-              )}
+              ))}
             </ul>
           </div>
         </div>
+      </>
+      {formData.customer_name && (
+        <>
+          <div className="mb-4">
+            <div className="flex flex-col w-max gap-2">
+              <label>Person</label>
+              <button
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                type="button"
+                onClick={() => setShowPersonDropDown(!showPersonDropDown)}
+              >
+                {formData.person_name || "Select Person"}
+                <svg
+                  className="w-2.5 h-2.5 ms-3"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 10 6"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="m1 1 4 4 4-4"
+                  />
+                </svg>
+              </button>
+            </div>
+            <div
+              className={`z-10 ${
+                !showPersonDropDown && "hidden"
+              } bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700`}
+            >
+              <ul className="py-2 text-sm text-gray-700 dark:text-gray-200">
+                {people.length > 0 ? (
+                  people.map((person) => (
+                    <li
+                      key={person._id}
+                      className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white cursor-pointer"
+                      onClick={() => {
+                        setFormData({ ...formData, person_name: person.name });
+                        setShowPersonDropDown(false);
+                      }}
+                    >
+                      {person.name}
+                    </li>
+                  ))
+                ) : (
+                  <li className="block px-4 py-2 text-gray-500">
+                    No people available for this customer
+                  </li>
+                )}
+              </ul>
+            </div>
+          </div>
+        </>
       )}
       <Input
         label="Message"
