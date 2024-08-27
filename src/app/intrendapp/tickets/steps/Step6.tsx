637efe201818b1d8fd8d263d9cf2f514b1577ea5
Ajit Shatvapa Gadkari <ajit@Ajits-MacBook-Pro.local>
2024-08-27 10:57:27 +0530

    step 6 update data feature added

--- src/app/intrendapp/tickets/steps/Step6.tsx
+++ src/app/intrendapp/tickets/steps/Step6.tsx
@@ -1,11 +1,10 @@
 import React, { useState } from "react";
 import Button from "../../../components/Button";
-import VendorDecodedMessage from "@/app/components/step6/VendorDecodedMessage";
 import { FaEdit } from "react-icons/fa";
 
 interface Step6Props {
   ticketNumber: string;
-  decodedMessages: { [key: string]: any };
+  decodedMessages: DecodedMessages;
   isCurrentStep: boolean;
   customerName: string;
   originalMessage: string;
@@ -57,9 +56,32 @@ const Step6: React.FC<Step6Props> = ({
   setActiveStep,
   ticket,
 }) => {
-  const [selectedMessages, setSelectedMessages] = useState<{ [key: string]: any }>({decodedMessages});
+  const [selectedMessages, setSelectedMessages] =
+    useState<DecodedMessages>(decodedMessages);
   const [loading, setLoading] = useState(false);
-  const [isEditing,setIsEditing] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const handleInputChange = (
+    vendor: string,
+    type: string,
+    field: keyof RateDetails | keyof ScheduleDetails,
+    value: string | number
+  ) => {
+    setSelectedMessages((prev) => {
+      const updated = { ...prev };
+      const vendorDetails = updated[vendor][type];
+
+      if (field in vendorDetails.rate) {
+        // TypeScript knows here that `field` must be a key of `RateDetails`
+        vendorDetails.rate[field as keyof RateDetails] = value as never;
+      } else if (field in vendorDetails.schedule) {
+        // TypeScript knows here that `field` must be a key of `ScheduleDetails`
+        vendorDetails.schedule[field as keyof ScheduleDetails] = value as never;
+      }
+
+      return updated;
+    });
+  };
 
   const handleNext = async () => {
     console.log("Handling next for Step 6");
@@ -68,7 +90,7 @@ const Step6: React.FC<Step6Props> = ({
     setActiveStep("Step 7 : Customer Message Template");
   };
 
-  const handleUpdate = async (updatedDecodedMessages: { [key: string]: any }) => {
+  const handleUpdate = async (updatedDecodedMessages: DecodedMessages) => {
     console.log("Updating Step 6 messages:", updatedDecodedMessages);
     await fetch(
       `${process.env.NEXT_PUBLIC_ENDPOINT_URL}/ticket/update_specific_step/`,
@@ -80,13 +102,13 @@ const Step6: React.FC<Step6Props> = ({
         body: JSON.stringify({
           ticket_number: ticket.ticket_number,
           step_number: "Step 6 : Vendor Message Decoded",
-          step_info: JSON.stringify(updatedDecodedMessages),
+          step_info: updatedDecodedMessages,
         }),
       }
     );
     fetchTicket(ticket._id);
   };
-  console.log(decodedMessages)
+  console.log(selectedMessages);
   const handleSelectChange = (vendor: string, isChecked: boolean) => {
     setSelectedMessages((prev) => {
       const updated = { ...prev };
@@ -102,10 +124,9 @@ const Step6: React.FC<Step6Props> = ({
   const handleNextStep = async () => {
     try {
       setLoading(true);
-
+      console.log(selectedMessages);
       // Update the current step with selected messages
-   
-      await handleUpdate(selectedMessages)
+      await handleUpdate(selectedMessages);
 
       // Generate client message template
       const response = await fetch(
@@ -152,13 +173,19 @@ const Step6: React.FC<Step6Props> = ({
       console.error("Error preparing for next step:", error);
     }
   };
+
   return (
     <div>
       <h3>Decoded Messages from Vendors</h3>
-      <div className='flex justify-end items-center' > <FaEdit className='text-black text-2xl' onClick={()=>setIsEditing(true)}/></div>
+      <div className="flex justify-end items-center"  onClick={() => setIsEditing(!isEditing)}>
+        <FaEdit
+          className="text-black text-2xl"
+         
+        />
+      </div>
       {!loading &&
-        Object.keys(decodedMessages).map((vendor) => {
-          const vendorData = decodedMessages[vendor];
+        Object.keys(selectedMessages).map((vendor) => {
+          const vendorData = selectedMessages[vendor];
 
           return (
             <div key={vendor} className="mb-4">
@@ -172,16 +199,174 @@ const Step6: React.FC<Step6Props> = ({
                     <div className="ml-4 flex md:flex-row flex-col md: gap-3">
                       <div className="bg-gray-100 px-3 py-2 rounded-md">
                         <h5 className="font-medium">Rate:</h5>
-                        <p>Price per meter: {vendorDetails.rate.price_per_meter} {vendorDetails.rate.currency}</p>
-                        <p>Quantity: {vendorDetails.rate.quantity}</p>
-                        <p>Additional Charges: {vendorDetails.rate.additional_charges}</p>
-                        <p>Other Info: {vendorDetails.rate.other_info}</p>
+                        {isEditing ? (
+                          <>
+                            <p>
+                              Price per meter:
+                              <input
+                                type="number"
+                                value={vendorDetails.rate.price_per_meter}
+                                onChange={(e) =>
+                                  handleInputChange(
+                                    vendor,
+                                    type,
+                                    "price_per_meter",
+                                    parseFloat(e.target.value)
+                                  )
+                                }
+                                className="ml-2 border rounded pl-2"
+                              />
+                            </p>
+                            <p>
+                              Currency:
+                              <input
+                                type="text"
+                                value={vendorDetails.rate.currency}
+                                onChange={(e) =>
+                                  handleInputChange(
+                                    vendor,
+                                    type,
+                                    "currency",
+                                    e.target.value
+                                  )
+                                }
+                                className="ml-2 border rounded pl-2"
+                              />
+                            </p>
+                            <p>
+                              Quantity:
+                              <input
+                                type="number"
+                                value={vendorDetails.rate.quantity}
+                                onChange={(e) =>
+                                  handleInputChange(
+                                    vendor,
+                                    type,
+                                    "quantity",
+                                    parseFloat(e.target.value)
+                                  )
+                                }
+                                className="ml-2 border rounded pl-2"
+                              />
+                            </p>
+                            <p>
+                              Additional Charges:
+                              <input
+                                type="text"
+                                value={vendorDetails.rate.additional_charges}
+                                onChange={(e) =>
+                                  handleInputChange(
+                                    vendor,
+                                    type,
+                                    "additional_charges",
+                                    e.target.value
+                                  )
+                                }
+                                className="ml-2 border rounded pl-2"
+                              />
+                            </p>
+                            <p>
+                              Other Info:
+                              <input
+                                type="text"
+                                value={vendorDetails.rate.other_info}
+                                onChange={(e) =>
+                                  handleInputChange(
+                                    vendor,
+                                    type,
+                                    "other_info",
+                                    e.target.value
+                                  )
+                                }
+                                className="ml-2 border rounded"
+                              />
+                            </p>
+                          </>
+                        ) : (
+                          <>
+                            <p>
+                              Price per meter:{" "}
+                              {vendorDetails.rate.price_per_meter}{" "}
+                              {vendorDetails.rate.currency}
+                            </p>
+                            <p>Quantity: {vendorDetails.rate.quantity}</p>
+                            <p>
+                              Additional Charges:{" "}
+                              {vendorDetails.rate.additional_charges}
+                            </p>
+                            <p>Other Info: {vendorDetails.rate.other_info}</p>
+                          </>
+                        )}
                       </div>
                       <div className="bg-gray-100 px-3 py-2 rounded-md">
                         <h5 className="font-medium">Schedule:</h5>
-                        <p>Delivery Method: {vendorDetails.schedule.delivery_method}</p>
-                        <p>Delivery Time: {vendorDetails.schedule.delivery_time}</p>
-                        <p>Delivery Point: {vendorDetails.schedule.delivery_point}</p>
+                        {isEditing ? (
+                          <>
+                            <p>
+                              Delivery Method:
+                              <input
+                                type="text"
+                                value={vendorDetails.schedule.delivery_method}
+                                onChange={(e) =>
+                                  handleInputChange(
+                                    vendor,
+                                    type,
+                                    "delivery_method",
+                                    e.target.value
+                                  )
+                                }
+                                className="ml-2 border rounded pl-2"
+                              />
+                            </p>
+                            <p>
+                              Delivery Time:
+                              <input
+                                type="text"
+                                value={vendorDetails.schedule.delivery_time}
+                                onChange={(e) =>
+                                  handleInputChange(
+                                    vendor,
+                                    type,
+                                    "delivery_time",
+                                    e.target.value
+                                  )
+                                }
+                                className="ml-2 border rounded pl-2"
+                              />
+                            </p>
+                            <p>
+                              Delivery Point:
+                              <input
+                                type="text"
+                                value={vendorDetails.schedule.delivery_point}
+                                onChange={(e) =>
+                                  handleInputChange(
+                                    vendor,
+                                    type,
+                                    "delivery_point",
+                                    e.target.value
+                                  )
+                                }
+                                className="ml-2 border rounded pl-2"
+                              />
+                            </p>
+                          </>
+                        ) : (
+                          <>
+                            <p>
+                              Delivery Method:{" "}
+                              {vendorDetails.schedule.delivery_method}
+                            </p>
+                            <p>
+                              Delivery Time:{" "}
+                              {vendorDetails.schedule.delivery_time}
+                            </p>
+                            <p>
+                              Delivery Point:{" "}
+                              {vendorDetails.schedule.delivery_point}
+                            </p>
+                          </>
+                        )}
                       </div>
                     </div>
                   </div>
