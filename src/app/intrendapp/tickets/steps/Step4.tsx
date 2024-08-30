c30326062ca5fb675e2c8068005ea3a562ebb33c
Ajit Shatvapa Gadkari <ajit@Ajits-MacBook-Pro.local>
2024-08-30 23:11:07 +0530

    all steps working

--- src/app/intrendapp/tickets/steps/Step4.tsx
+++ src/app/intrendapp/tickets/steps/Step4.tsx
@@ -55,17 +55,18 @@ const Step4: React.FC<Step4Props> = ({
   const [vendorDetails, setVendorDetails] = useState<Vendor[]>([]);
   const [isEmailSending, setIsEmailSending] = useState(false);
   const [isWhatsAppSending, setIsWhatsAppSending] = useState(false);
-  const [emailSendingStatus, setEmailSendingStatus] = useState<Record<string, string>>(
-    {}
-  );
-  const [whatsappSendingStatus, setWhatsappSendingStatus] = useState<Record<string, string>>(
-    {}
-  );
+  const [emailSendingStatus, setEmailSendingStatus] = useState<
+    Record<string, string>
+  >({});
+  const [whatsappSendingStatus, setWhatsappSendingStatus] = useState<
+    Record<string, string>
+  >({});
   const [isMessageSent, setIsMessageSent] = useState({
     emailSent: false,
     whatsappMessageSent: false,
   });
-  const [showResendWhatsappMessage,setShowResendWhatsappMessage]=useState(false);
+  const [showResendWhatsappMessage, setShowResendWhatsappMessage] =
+    useState(false);
   const [showResendEmail, setShowResendEmail] = useState(false);
 
   const [loading, setLoading] = useState(false);
@@ -100,7 +101,7 @@ const Step4: React.FC<Step4Props> = ({
       );
 
       console.log("Step 5 initial vendor messages:", emptyVendorMessages);
-  
+
       await fetch(
         `${process.env.NEXT_PUBLIC_ENDPOINT_URL}/ticket/update_next_step/`,
         {
@@ -244,15 +245,16 @@ const Step4: React.FC<Step4Props> = ({
         toast.error("Email already sent!");
         setShowResendEmail(true);
       }
-    }else{
+    } else {
       setIsMessageSent((prev) => {
         const updated = { ...prev, emailSent: true };
         return updated;
       });
+      const successfullySentVendors: Record<string, string> = {};
       for (const option of selectedOptions) {
         console.log(option);
         const vendor = vendorDetails.find((v) => v.name === option.value);
-        
+
         if (vendor) {
           try {
             console.log(vendor.email);
@@ -272,10 +274,19 @@ const Step4: React.FC<Step4Props> = ({
             );
             const data = await response.json();
             console.log(`Email sent to ${option.value}:`, data);
-            setEmailSendingStatus((prev) => ({ ...prev, [option.value]: "Email Sent" }));
+            setEmailSendingStatus((prev) => ({
+              ...prev,
+              [option.value]: "Email Sent",
+            }));
+            successfullySentVendors[option.value] =
+              vendorMessages[option.value];
+            console.log(successfullySentVendors);
           } catch (error) {
             console.error(`Error sending email to ${option.value}:`, error);
-            setEmailSendingStatus((prev) => ({ ...prev, [option.value]: "Failed" }));
+            setEmailSendingStatus((prev) => ({
+              ...prev,
+              [option.value]: "Failed",
+            }));
             setIsMessageSent((prev) => {
               const updated = { ...prev, emailSent: false };
               return updated;
@@ -292,91 +303,174 @@ const Step4: React.FC<Step4Props> = ({
             return updated;
           });
         }
-      } 
+      }
+      await fetch(
+        `${process.env.NEXT_PUBLIC_ENDPOINT_URL}/ticket/update_specific_step/`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            ticket_number: ticket.ticket_number,
+            step_number: "Step 4 : Vendor Selection",
+            step_info: { list: Object.keys(vendorMessages) },
+          }),
+        }
+      );
+
+      // Initialize Step 5 with empty messages
+      const emptyVendorMessages = Object.keys(vendorMessages).reduce(
+        (acc, vendor) => {
+          acc[vendor] = "";
+          return acc;
+        },
+        {} as Record<string, string>
+      );
+
+      console.log("Step 5 initial vendor messages:", emptyVendorMessages);
+
+      await fetch(
+        `${process.env.NEXT_PUBLIC_ENDPOINT_URL}/ticket/update_next_step/`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            ticket_number: ticket.ticket_number,
+            step_info: emptyVendorMessages,
+            step_number: "Step 5: Messages from Vendors",
+          }),
+        }
+      );
     }
     setIsEmailSending(false);
-    };
-    
-    const handlePopupConfirm = async () => {
-      setShowPopup(false);
-      setIsWhatsAppSending(true);
-      setWhatsappSendingStatus({});
-    if(isMessageSent.whatsappMessageSent){
-      if(!showResendWhatsappMessage){
-      toast.error("Message already sent!")
-      setShowResendWhatsappMessage(true)
-      }
-    }else{
-    setIsMessageSent((prev) => {
-      const updated = { ...prev, whatsappMessageSent: true };
-      return updated;
-    });
+  };
 
-    for (const option of selectedOptions) {
-      const vendor = vendorDetails.find((v) => v.name === option.value);
-      if (vendor && vendor.group) {
-        const groupId =
-          typeof vendor.group === "string"
-            ? vendor.group
-            : Object.values(vendor.group)[0];
-        if (groupId) {
-          try {
-            const response = await fetch(
-              `${process.env.NEXT_PUBLIC_ENDPOINT_URL}/send_whatsapp_message/`,
-              {
-                method: "POST",
-                headers: {
-                  "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                  to: groupId,
-                  message: vendorMessages[option.value],
-                }),
-              }
-            );
-            const data = await response.json();
-            console.log(`Message sent to ${option.value}:`, data);
+  const handlePopupConfirm = async () => {
+    setShowPopup(false);
+    setIsWhatsAppSending(true);
+    setWhatsappSendingStatus({});
+    if (isMessageSent.whatsappMessageSent) {
+      if (!showResendWhatsappMessage) {
+        toast.error("Message already sent!");
+        setShowResendWhatsappMessage(true);
+      }
+    } else {
+      setIsMessageSent((prev) => {
+        const updated = { ...prev, whatsappMessageSent: true };
+        return updated;
+      });
+      const successfullySentVendors: Record<string, string> = {};
+      for (const option of selectedOptions) {
+        const vendor = vendorDetails.find((v) => v.name === option.value);
+        if (vendor && vendor.group) {
+          const groupId =
+            typeof vendor.group === "string"
+              ? vendor.group
+              : Object.values(vendor.group)[0];
+          if (groupId) {
+            try {
+              const response = await fetch(
+                `${process.env.NEXT_PUBLIC_ENDPOINT_URL}/send_whatsapp_message/`,
+                {
+                  method: "POST",
+                  headers: {
+                    "Content-Type": "application/json",
+                  },
+                  body: JSON.stringify({
+                    to: groupId,
+                    message: vendorMessages[option.value],
+                  }),
+                }
+              );
+              const data = await response.json();
+              console.log(`Message sent to ${option.value}:`, data);
+              setWhatsappSendingStatus((prev) => ({
+                ...prev,
+                [option.value]: "Whatsapp Message Sent",
+              }));
+              successfullySentVendors[option.value] =
+                vendorMessages[option.value];
+              console.log(successfullySentVendors);
+            } catch (error) {
+              console.error(`Error sending message to ${option.value}:`, error);
+              setWhatsappSendingStatus((prev) => ({
+                ...prev,
+                [option.value]: "Failed",
+              }));
+              setIsMessageSent((prev) => {
+                const updated = { ...prev, whatsappMessageSent: false };
+                return updated;
+              });
+            }
+          } else {
+            console.error(`No group ID found for vendor ${option.value}`);
             setWhatsappSendingStatus((prev) => ({
               ...prev,
-              [option.value]: "Whatsapp Message Sent",
-            }));
-          } catch (error) {
-            console.error(`Error sending message to ${option.value}:`, error);
-            setWhatsappSendingStatus((prev) => ({
-              ...prev,
-              [option.value]: "Failed",
+              [option.value]: "Vendor details not found",
             }));
             setIsMessageSent((prev) => {
-              const updated = { ...prev,whatsappMessageSent: false };
+              const updated = { ...prev, whatsappMessageSent: false };
               return updated;
             });
           }
         } else {
-          console.error(`No group ID found for vendor ${option.value}`);
+          console.error(`Vendor details not found for ${option.value}`);
           setWhatsappSendingStatus((prev) => ({
             ...prev,
             [option.value]: "Vendor details not found",
           }));
           setIsMessageSent((prev) => {
-            const updated = { ...prev,whatsappMessageSent: false };
-            return updated;
+            return { ...prev, whatsappMessageSent: false };
           });
         }
-      } else {
-        console.error(`Vendor details not found for ${option.value}`);
-        setWhatsappSendingStatus((prev) => ({
-          ...prev,
-          [option.value]: "Vendor details not found",
-        }));
-        setIsMessageSent((prev) => {
-          return { ...prev, whatsappMessageSent: false };
-        });
       }
-      }
-    }
-  
-  setIsWhatsAppSending(false);
+      console.log(successfullySentVendors);
+      // Save Step 4 data
+      await fetch(
+        `${process.env.NEXT_PUBLIC_ENDPOINT_URL}/ticket/update_specific_step/`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            ticket_number: ticket.ticket_number,
+            step_number: "Step 4 : Vendor Selection",
+            step_info: { list: Object.keys(successfullySentVendors) },
+          }),
+        }
+      );
 
+      // Initialize Step 5 with empty messages
+      const emptyVendorMessages = Object.keys(successfullySentVendors).reduce(
+        (acc, vendor) => {
+          acc[vendor] = "";
+          return acc;
+        },
+        {} as Record<string, string>
+      );
+
+      console.log("Step 5 initial vendor messages:", emptyVendorMessages);
+
+      await fetch(
+        `${process.env.NEXT_PUBLIC_ENDPOINT_URL}/ticket/update_next_step/`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            ticket_number: ticket.ticket_number,
+            step_info: emptyVendorMessages,
+            step_number: "Step 5: Messages from Vendors",
+          }),
+        }
+      );
+    }
+    setIsWhatsAppSending(false);
   };
 
   return (
@@ -416,7 +510,8 @@ const Step4: React.FC<Step4Props> = ({
                   {whatsappSendingStatus[option.value] && (
                     <p
                       className={`mt-1 ${
-                        whatsappSendingStatus[option.value] === "Whatsapp Message Sent"
+                        whatsappSendingStatus[option.value] ===
+                        "Whatsapp Message Sent"
                           ? "text-green-600"
                           : "text-red-600"
                       }`}
@@ -439,28 +534,46 @@ const Step4: React.FC<Step4Props> = ({
             <Button
               onClick={handleSendMessage}
               className={`bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded flex items-center gap-2 ${
-                (!isCurrentStep || selectedOptions.length === 0 || isWhatsAppSending) &&
+                (!isCurrentStep ||
+                  selectedOptions.length === 0 ||
+                  isWhatsAppSending) &&
                 "opacity-50 cursor-not-allowed"
               }`}
               disabled={
-                !isCurrentStep || selectedOptions.length === 0 || isWhatsAppSending
+                !isCurrentStep ||
+                selectedOptions.length === 0 ||
+                isWhatsAppSending
               }
             >
-             <span>{isWhatsAppSending ? "Sending..." : showResendWhatsappMessage ? "Resend" : "Send"}</span>
+              <span>
+                {isWhatsAppSending
+                  ? "Sending..."
+                  : showResendWhatsappMessage
+                  ? "Resend"
+                  : "Send"}
+              </span>
 
               <FaWhatsapp />
             </Button>
             <Button
               onClick={handleSendEmail}
               className={`bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded flex items-center gap-2 ${
-                (!isCurrentStep || selectedOptions.length === 0 || isEmailSending) &&
+                (!isCurrentStep ||
+                  selectedOptions.length === 0 ||
+                  isEmailSending) &&
                 "opacity-50 cursor-not-allowed "
               }`}
               disabled={
                 !isCurrentStep || selectedOptions.length === 0 || isEmailSending
               }
             >
-            <span>{isEmailSending ? "Sending..." : showResendEmail ? "Resend" : "Send"}</span>
+              <span>
+                {isEmailSending
+                  ? "Sending..."
+                  : showResendEmail
+                  ? "Resend"
+                  : "Send"}
+              </span>
               <MdEmail />
             </Button>
             <Button
@@ -483,12 +596,16 @@ const Step4: React.FC<Step4Props> = ({
         <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full flex items-center justify-center">
           <div className="bg-white p-5 rounded-lg shadow-xl">
             <h2 className="text-xl font-bold mb-4">Sending Messages...</h2>
-            {!showResendWhatsappMessage&&<p className="mb-4">
-              Are you sure you want to send messages to the selected vendors?
-            </p>}
+            {!showResendWhatsappMessage && (
+              <p className="mb-4">
+                Are you sure you want to send messages to the selected vendors?
+              </p>
+            )}
             {showResendWhatsappMessage && (
-            <p>Messages have already been sent. Do you want to resend them?</p>
-          )}
+              <p>
+                Messages have already been sent. Do you want to resend them?
+              </p>
+            )}
             <div className="flex justify-end">
               <Button
                 onClick={() => setShowPopup(false)}
@@ -500,39 +617,41 @@ const Step4: React.FC<Step4Props> = ({
                 onClick={handlePopupConfirm}
                 className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
               >
-               {!showResendWhatsappMessage ? "OK" : "Resend"}
+                {!showResendWhatsappMessage ? "OK" : "Resend"}
               </Button>
             </div>
           </div>
         </div>
       )}
-     {showEmailPopup && (
-  <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full flex items-center justify-center">
-    <div className="bg-white p-5 rounded-lg shadow-xl">
-      <h2 className="text-xl font-bold mb-4">Sending Emails...</h2>
-      {!showResendEmail && (
-        <p className="mb-4">Are you sure you want to send emails to the selected vendors?</p>
+      {showEmailPopup && (
+        <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full flex items-center justify-center">
+          <div className="bg-white p-5 rounded-lg shadow-xl">
+            <h2 className="text-xl font-bold mb-4">Sending Emails...</h2>
+            {!showResendEmail && (
+              <p className="mb-4">
+                Are you sure you want to send emails to the selected vendors?
+              </p>
+            )}
+            {showResendEmail && (
+              <p>Emails have already been sent. Do you want to resend them?</p>
+            )}
+            <div className="flex justify-end">
+              <Button
+                onClick={() => setShowEmailPopup(false)}
+                className="mr-2 bg-gray-300 hover:bg-gray-400 text-black font-bold py-2 px-4 rounded"
+              >
+                Cancel
+              </Button>
+              <Button
+                onClick={handleEmailPopupConfirm}
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              >
+                {!showResendEmail ? "OK" : "Resend"}
+              </Button>
+            </div>
+          </div>
+        </div>
       )}
-      {showResendEmail && (
-        <p>Emails have already been sent. Do you want to resend them?</p>
-      )}
-      <div className="flex justify-end">
-        <Button
-          onClick={() => setShowEmailPopup(false)}
-          className="mr-2 bg-gray-300 hover:bg-gray-400 text-black font-bold py-2 px-4 rounded"
-        >
-          Cancel
-        </Button>
-        <Button
-          onClick={handleEmailPopupConfirm}
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        >
-          {!showResendEmail ? "OK" : "Resend"}
-        </Button>
-      </div>
-    </div>
-  </div>
-)}
       {!isCurrentStep && (
         <h1 className="text-end text-yellow-500">Messages Already Sent!</h1>
       )}
